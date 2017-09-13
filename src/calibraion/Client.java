b12cfada3441332cd278167280e667627496f2f8
Franciszek <franciszek.stepien@gmail.com>
2017-09-13 19:48:58 +0200

    version v2.0

---
+++ src/calibraion/Client.java
@@ -0,0 +1,12 @@
+package calibraion;
+
+//dane klienta
+class Client{
+  String name;
+  String address;
+  String postalCode;
+  String place;
+  public String toString(){
+      return address+", "+postalCode+" "+place;
+  }
+}
